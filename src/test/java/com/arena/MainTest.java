3789891169676d20a8e07d2234c1537b9c215d7c
A.SALLIER <sallierdupin.arthur.pro@gmail.com>
2025-05-18 17:14:21 +0200

    Main remove SET UTF8
    UTF8 Base was created to ensure logs are displayed in UTF8 but surefire handle it properly, UTF8 not necessary.

--- src/test/java/com/arena/MainTest.java
+++ src/test/java/com/arena/MainTest.java
@@ -3,7 +3,7 @@ package com.arena;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-public class MainTest extends UTF8Base {
+public class MainTest {
     @Test
     void mainRunWithoutException() {
         assertDoesNotThrow(() -> Main.main(new String[]{}), "Main method should run without throwing an exception");
