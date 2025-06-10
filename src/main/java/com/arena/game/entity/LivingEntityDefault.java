eb24b565f26f55d205cecaf4d282ad7013b1a1d4
A.SALLIER <sallierdupin.arthur.pro@gmail.com>
2025-06-10 20:22:03 +0200

    Skin nexus inhib

--- src/main/java/com/arena/game/entity/LivingEntityDefault.java
+++
@@ -1,9 +0,0 @@
-package com.arena.game.entity;
-
-import com.arena.utils.Vector3f;
-
-public class LivingEntityDefault extends LivingEntity {
-    public LivingEntityDefault(String id, int team) {
-        super(id, 600, team, "Default"); // maxHealth
-    }
-}
